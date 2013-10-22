78701b2425dfa5d95289e3e70d2b0f527d08e932
Buu Nguyen <buunguyen@gmail.com>
2013-10-22 11:26:08 -0500

    2.2.2.17

--- Combres.Log4Net/Properties/AssemblyInfo.cs
+++ Combres.Log4Net/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.2.15")]
-[assembly: AssemblyFileVersion("2.2.2.15")]
+[assembly: AssemblyVersion("2.2.2.17")]
+[assembly: AssemblyFileVersion("2.2.2.17")]
